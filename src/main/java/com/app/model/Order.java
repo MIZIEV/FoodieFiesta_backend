045a64ed5df3178d8c55b7748ee7fdb311a72fb5
Miziev <mizzzev@outlook.com>
2024-07-25 13:18:36 +0300

    - created Order entity (without all fields)

---
+++ src/main/java/com/app/model/Order.java
@@ -0,0 +1,18 @@
+package com.app.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "orders")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
