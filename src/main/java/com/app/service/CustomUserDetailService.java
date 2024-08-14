2d940344f65f71e91588a1ef782a65f8fdea987c
Miziev <mizzzev@outlook.com>
2024-08-14 18:04:34 +0300

    - created custom userDetailService - added default role to User entity

---
+++ src/main/java/com/app/service/CustomUserDetailService.java
@@ -0,0 +1,46 @@
+package com.app.service;
+
+import com.app.model.Role;
+import com.app.model.User;
+import com.app.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+
+        if (user != null) {
+            throw new UsernameNotFoundException("User " + username + " not found");
+        }
+        Role role = user.getRole();
+
+        if (role == null) {
+            role = Role.CUSTOMER_ROLE;
+        }
+
+        List<GrantedAuthority> authorityList = new ArrayList<>();
+        authorityList.add(new SimpleGrantedAuthority(role.toString()));
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(),
+                user.getPassword(), authorityList);
+    }
+}
