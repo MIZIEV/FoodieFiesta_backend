6f3870fa0168b73330876bf87ca531f43b14ea37
Miziev <mizzzev@outlook.com>
2024-08-29 12:27:21 +0300

    - created AuthResponse entity - added Service annotation for jwtProvider - created register method in controller

--- src/main/java/com/app/config/JwtProvider.java
+++ src/main/java/com/app/config/JwtProvider.java
@@ -6,6 +6,8 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.util.Collection;
@@ -13,6 +15,7 @@ import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
+@Service
 public class JwtProvider {
 
     private SecretKey key = Keys.hmacShaKeyFor(JwtConstant.JWT_SECRET.getBytes());
--- src/main/java/com/app/controller/AuthController.java
+++ src/main/java/com/app/controller/AuthController.java
@@ -1,11 +1,21 @@
 package com.app.controller;
 
 import com.app.config.JwtProvider;
+import com.app.dto.AuthResponse;
+import com.app.model.Cart;
+import com.app.model.User;
 import com.app.repository.CartRepository;
 import com.app.repository.UserRepository;
 import com.app.service.CustomUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,6 +43,38 @@ public class AuthController {
         this.jwtProvider = jwtProvider;
         this.customUserDetailService = customUserDetailService;
         this.cartRepository = cartRepository;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> createUser(@RequestBody User user) throws Exception {
+
+        User isUserExist = userRepository.findByEmail(user.getEmail());
+
+        if (isUserExist != null) {
+            throw new Exception("Email is already used!");
+        }
+
+        User newUser = new User();
+        newUser.setEmail(user.getEmail());
+        newUser.setFullName(user.getFullName());
+        newUser.setRole(user.getRole());
+        newUser.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        User savedUser = userRepository.save(newUser);
+        Cart cart = new Cart();
+        cart.setCustomer(savedUser);
+        cartRepository.save(cart);
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = jwtProvider.generateToken(authentication);
+        AuthResponse authResponse = new AuthResponse();
+        authResponse.setToken(jwt);
+        authResponse.setMessage("Registration Successful");
+        authResponse.setRole(savedUser.getRole());
+
+        return new ResponseEntity<>(authResponse, HttpStatus.CREATED);
     }
 
 
