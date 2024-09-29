a944ede2a67441bad179a39b8f6bf97dbb353454
Miziev <mizzzev@outlook.com>
2024-09-29 20:12:36 +0300

    - created repository for Address - implemented create method in restaurant service

--- src/main/java/com/app/service/impl/RestaurantServiceImpl.java
+++ src/main/java/com/app/service/impl/RestaurantServiceImpl.java
@@ -1,29 +1,52 @@
 package com.app.service.impl;
 
 import com.app.dto.RestaurantDto;
+import com.app.model.Address;
 import com.app.model.Restaurant;
 import com.app.model.User;
+import com.app.repository.AddressRepository;
 import com.app.repository.RestaurantRepository;
 import com.app.request.CreateRestaurantRequest;
 import com.app.service.RestaurantService;
+import com.app.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 public class RestaurantServiceImpl implements RestaurantService {
 
     private final RestaurantRepository restaurantRepository;
+    private final AddressRepository addressRepository;
+    private final UserService userService;
 
     @Autowired
-    public RestaurantServiceImpl(RestaurantRepository restaurantRepository) {
+    public RestaurantServiceImpl(RestaurantRepository restaurantRepository, AddressRepository addressRepository, UserService userService) {
         this.restaurantRepository = restaurantRepository;
+        this.addressRepository = addressRepository;
+        this.userService = userService;
     }
 
     @Override
     public Restaurant createRestaurant(CreateRestaurantRequest request, User user) {
-        return null;
+
+        Address address = addressRepository.save(request.getAddress());
+        Restaurant restaurant = new Restaurant();
+
+        restaurant.setAddress(address);
+        restaurant.setContactInformation(request.getContactInformation());
+        restaurant.setCuisineType(request.getCuisineType());
+        restaurant.setDescription(request.getDescription());
+        restaurant.setImages(request.getImages());
+        restaurant.setName(request.getName());
+        restaurant.setOpeningHours(request.getOpeningHours());
+        restaurant.setRegistrationDate(LocalDateTime.now());
+        restaurant.setOwner(user);
+
+        restaurantRepository.save(restaurant);
+        return restaurant;
     }
 
     @Override
